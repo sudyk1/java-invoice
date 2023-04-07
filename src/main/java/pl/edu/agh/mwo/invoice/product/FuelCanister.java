953b4c9b75f11d976f9ed891f113a995c2ae28cb
sudyk1 <108087334+sudyk1@users.noreply.github.com>
2023-04-07 17:10:49 +0200

    Fixing checkstyle2

--- src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/FuelCanister.java
@@ -1,14 +1,12 @@
 package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
-import java.time.Clock;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.function.Supplier;
 
 public class FuelCanister extends Product {
-    private final int MARCH = 3;
-    private final int MARCH_FIVE = 5;
+    private final int march = 3;
+    private final int day = 5;
     private final BigDecimal excise = new BigDecimal("5.56");
 
     public static Supplier<LocalDateTime> localDateTime = () -> LocalDateTime.now();
@@ -19,7 +17,8 @@ public class FuelCanister extends Product {
 
     @Override
     public BigDecimal getPriceWithTax() {
-        if (localDateTime.get().getDayOfMonth() == MARCH_FIVE && localDateTime.get().getMonthValue() == MARCH) {
+        if (localDateTime.get().getDayOfMonth() == day
+                && localDateTime.get().getMonthValue() == march) {
             return getPrice().add(excise);
         } else {
             return super.getPriceWithTax().add(excise);
